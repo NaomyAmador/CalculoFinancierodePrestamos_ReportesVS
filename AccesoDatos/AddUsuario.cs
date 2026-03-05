50bf37bcfed2df663fdf7cb6c5bc12acf23fc0a7
naomyamador_04 <naomyabigail2009@gmail.com>
2026-03-04 21:20:35 -0400

    Clase AddUsuario para insertar nuevos usuarios creada en AccesoDatos

---
+++ AccesoDatos/AddUsuario.cs
@@ -0,0 +1,27 @@
+﻿using Entidades;
+using System;
+using System.Data.SqlClient;
+
+namespace AccesoDatos
+{
+    public class AddUsuario
+    {
+        ConexiónBDD conexióBDD = new ConexiónBDD();
+        public int InsertarUsuario(User_Login DatosUsuario)
+        {
+            using (SqlConnection Conexión = conexióBDD.ObtenerConexión())
+            {
+                Conexión.Open();
+                string Consulta = @"INSERT INTO User_Login (Usuario, Contraseña) VALUES (@Usuario, @Contraseña); SELECT SCOPE_IDENTITY();";
+
+                SqlCommand AgregarUsuario = new SqlCommand(Consulta, Conexión);
+
+                AgregarUsuario.Parameters.AddWithValue("@Usuario", DatosUsuario.Usuario);
+                AgregarUsuario.Parameters.AddWithValue("@Contraseña", DatosUsuario.Contraseña);
+
+                int IdAgregado = Convert.ToInt32(AgregarUsuario.ExecuteScalar());
+                return IdAgregado;
+            }
+        }
+    }
+}
