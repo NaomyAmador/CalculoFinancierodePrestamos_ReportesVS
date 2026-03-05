16aca8b4ea47bc54ed62601df113fd4314c8b61c
naomyamador_04 <naomyabigail2009@gmail.com>
2026-03-04 20:09:14 -0400

    Cadena de Conexión a la BDD hecha en AccesoDatos

---
+++ AccesoDatos/ConexiónBDD.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AccesoDatos
+{
+    public class ConexiónBDD
+    {
+        private string CadenaConexión = "server=LAPTOP-9G07MQQC\\SQLEXPRESS; database=CalculoFinancierodeReportes;; integrated security=true;";
+
+        public SqlConnection ObtenerConexión()
+        {
+            SqlConnection Conexión = new SqlConnection(CadenaConexión);
+            Conexión.Open();
+            return Conexión;
+        }
+    }
+}
