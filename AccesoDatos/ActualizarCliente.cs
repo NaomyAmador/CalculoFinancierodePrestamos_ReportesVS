ed0314af2df4ef16c097aa1a650a2e2422d3a933
Lia Karielys Torres Dominici <Dominicilia1@gmail.com>
2026-03-11 13:19:33 -0400

    Creacion de acceso a datos clientes (Comprobación usuario y actualización)

---
+++ AccesoDatos/ActualizarCliente.cs
@@ -0,0 +1,34 @@
+﻿using Entidades;
+using System.Data.SqlClient;
+
+
+namespace AccesoDatos
+{
+    public  class ActualizarCliente
+    {
+        ConexiónBDD conexiónBDD = new ConexiónBDD();
+        public int ActualizarClientes(Clientes DatosClientes)
+        {
+            using (SqlConnection Conexión = conexiónBDD.ObtenerConexión())
+            {
+                string Consulta = @"UPDATE Clientes SET NombreCompleto = @NombreCompleto, Correo = @Correo, Direccion = @Direccion, Telefono = @Telefono, SueldoMensual = @SueldoMensual WHERE IdCliente = @IdCliente";
+
+
+                SqlCommand ActualizarCliente = new SqlCommand(Consulta, Conexión);
+
+                ActualizarCliente.Parameters.AddWithValue("@IdCliente", DatosClientes.IdCliente);
+                ActualizarCliente.Parameters.AddWithValue("@NombreCompleto", DatosClientes.NombreCompleto);
+                ActualizarCliente.Parameters.AddWithValue("@Correo", DatosClientes.Correo);
+                ActualizarCliente.Parameters.AddWithValue("@Direccion", DatosClientes.Direccion);
+                ActualizarCliente.Parameters.AddWithValue("@Telefono", DatosClientes.Telefono);
+                ActualizarCliente.Parameters.AddWithValue("@SueldoMensual", DatosClientes.SueldoMensual);
+
+                int ClienteActualizado = ActualizarCliente.ExecuteNonQuery();
+
+                return ClienteActualizado;
+                              
+            }
+        }
+
+    }
+}
---
+++ AccesoDatos/ValidarCliente.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Data.SqlClient;
+
+
+namespace AccesoDatos
+{
+    public class ValidarCliente
+    {
+        ConexiónBDD ConexionBD = new ConexiónBDD();
+        public bool ValidarClientes(string IdCliente)
+        {
+            using (SqlConnection Conexión = ConexionBD.ObtenerConexión())
+            {
+                string Consulta = @"SELECT COUNT(*) FROM Clientes WHERE IdCliente = @IdCliente ";
+
+                SqlCommand ValidarClienteExistente = new SqlCommand(Consulta, Conexión);
+                ValidarClienteExistente.Parameters.AddWithValue("@IdCliente", IdCliente);
+                
+                int Validación = Convert.ToInt32(ValidarClienteExistente.ExecuteScalar());
+                return Validación > 0;
+            }
+
+        }
+    }
+}
